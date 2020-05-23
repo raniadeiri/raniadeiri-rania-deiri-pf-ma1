25a7301b1d73dbe4e71029f00c9788b97f50c8b1
raniadeiri <61733983+raniadeiri@users.noreply.github.com>
2020-05-23 21:57:21 +0200

    pf-ma1

---
+++ script.js
@@ -0,0 +1,51 @@
+// question 1
+
+var firstVar;
+
+
+// question 2
+
+var name;
+name = "rania";
+
+
+// question 3
+
+var Sum = 90;
+
+
+// question 4
+
+var division = (20/5);
+
+
+// question 5
+
+var animal = "frog";
+ 
+
+// question 6
+
+var orderHasShipped = true;
+
+
+
+// question 7
+
+var orderHasShipped = "true";
+
+if(orderHasShipped  == "true") {
+    console.log("true");
+}
+
+else
+{
+    console.log("false");
+};
+
+
+// question 8
+
+for (var i = 0; i <= 9; i += 1) { 
+  console.log(i);
+}
